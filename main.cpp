8cb1538467349ff170e524402b3ef6cdf4634f74
Troy <troy.w.miller.1994@gmail.com>
2023-02-13 22:25:14 -0600

    Adder and Multer

--- main.cpp
+++ main.cpp
@@ -11,15 +11,35 @@ int adder(std::vector<int> list) {
 	return output;
 }
 
+int multer(std::vector<int> list) {
+	int output = 1;
+	for (int i = 0; i < list.size()-1; i++) {//sizeof returns the memory size. -1 so we don't mult by zero.
+		output *= list[i];
+	}
+	return output;
+}
+
 int main() {
 	std::vector<int> list;
-	std::cout << "Add em up, zero to end: ";
 	int input = -1;
+	int mode = 0;
+	std::cout << "Add(1) or Mult(2): ";
+	std::cin >> mode;
+
+	std::cout << "Numbers please: /n";
 	while (input) {
 		std::cin >> input;
 		list.push_back(input);
 	}
-	int output = adder(list);
+	int output;
+	switch (mode) {
+	case 1:
+		output = adder(list);
+		break;
+	case 2:
+		output = multer(list);
+	}
+
 	std::cout << output;
 	return 0;
 }
